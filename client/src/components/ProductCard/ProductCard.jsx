b5b60733e59d5b507f06237fd8ecbec83512b8f4
Ddeltoro97 <davison09.ddt@gmail.com>
2024-05-15 00:15:07 -0500

    added star ratings

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,3 +1,4 @@
+import {Rate} from "antd";
 
 import styles from './ProductCard.module.css'
 
@@ -10,7 +11,12 @@ export default function ProductCard({product}){
             </div>
             <div className={styles.infoHolder}>
                 <h4>{product.name}</h4> 
-                <p>{product.rate}</p>
+                {/* <p>{product.rate}</p> */}
+                <Rate
+                allowHalf
+                allowClear={false}
+                disabled
+                defaultValue={product.rate}/>
                 <p>${product.price}</p>
             </div>
         </div>
